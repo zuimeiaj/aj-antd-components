bc5c44690ccf80150c8b7e3e076c515e357ed894
Jun Yao <zuimeiaj@outlook.com>
2023-07-20 18:09:32 +0800

    fx

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <script>
-import { LoadingButton } from 'aj-antd-components'
+import { FormLayout, LoadingButton } from 'aj-antd-components'
 export default {
   methods: {
     handleClick() {
@@ -11,7 +11,13 @@ export default {
     },
   },
   render() {
-    return <div class="app-page gutter"></div>
+    const columns = [{ label: '你好', type: 'input' }]
+    return (
+      <div class="app-page gutter">
+        <LoadingButton click={this.handleClick}>hello</LoadingButton>
+        <FormLayout columns={columns}></FormLayout>
+      </div>
+    )
   },
 }
 </script>
