c01048eb6b62f239b905c8df3c8df0ed07487542
Jun Yao <zuimeiaj@outlook.com>
2023-08-08 14:43:21 +0800

    修改打包配置

--- src/router.js
+++ src/router.js
@@ -4,12 +4,15 @@ import { ComponentList } from './components'
 vue.use(Router)
 const router = new Router({
   base: process.env.BASE_URL,
-  routes: ComponentList.map((item) => {
-    return {
-      path: '/' + item.toLowerCase(),
-      component: () => import('@/pages/' + item.toLowerCase() + '.vue'),
-    }
-  }),
+  routes: [
+    { path: '/', redirect: '/guide' },
+    ComponentList.map((item) => {
+      return {
+        path: '/' + item.toLowerCase(),
+        component: () => import('@/pages/' + item.toLowerCase() + '.vue'),
+      }
+    }),
+  ],
 })
 
 export default router
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir: 'biz',
   publicPath: process.env.NODE_ENV === 'production' ? '/biz/' : '/',
   css: {
     loaderOptions: {
