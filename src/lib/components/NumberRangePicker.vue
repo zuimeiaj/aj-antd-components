64e12932a9ac3865f0a8118c2a55054e191618cb
Jun Yao <2498683974@qq.com>
2024-03-31 08:29:25 +0800

    增加numberrangeplaceholder

--- src/lib/components/NumberRangePicker.vue
+++ src/lib/components/NumberRangePicker.vue
@@ -4,18 +4,21 @@ import { InputNumber } from 'ant-design-vue'
 export default {
   name: 'NumberRangePicker',
   props: {
+    placeholder: {},
     value: {
       default: () => [],
       type: Array,
     },
   },
   render() {
+    let placeholder = this.placeholder || []
     return (
       <div class="form-number-input">
         <InputNumber
           value={this.value[0]}
           onChange={(v) => this.$emit('change', [v, this.value[1]])}
           class="input-number"
+          placeholder={placeholder[0]}
           max={this.value[1] || 100000000}
           min={0}
         />
@@ -23,6 +26,7 @@ export default {
         <InputNumber
           onChange={(v) => this.$emit('change', [this.value[0], v])}
           class="input-number"
+          placeholder={placeholder[1]}
           value={this.value[1]}
           min={this.value[0] || 0}
         />
